c65e399da64d27ab52826d53b9c7e26d9d6d5710
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-11-15 12:21:40 +0000

    Refector of overviewpage into modular components

---
+++ client/src/components/CitySearch/utils.js
@@ -0,0 +1,47 @@
+import closeAllLists from "./closeAllLists";
+import styles from "./CitySearch.module.css";
+
+const populateAutocomplete = (
+    inputValue,
+    inputValueNotBlank,
+    citiesData,
+    clickOnSuggestionHandler
+) => {
+    closeAllLists();
+    if (inputValueNotBlank) {
+        const cityInput = document.getElementById("City");
+        const autoCompleteList = document.createElement("DIV");
+
+        autoCompleteList.setAttribute(
+            "id",
+            cityInput.id + "-autocomplete-list"
+        );
+        autoCompleteList.setAttribute("class", styles["autocomplete-items"]);
+
+        cityInput.parentNode.appendChild(autoCompleteList);
+
+        let autoCompleteLimit = 5;
+
+        citiesData.some((cityObj, i) => {
+            if (i >= autoCompleteLimit) return true;
+            let city = cityObj.city;
+            if (city === inputValue) return true;
+
+            let matchedCities = document.createElement("DIV");
+
+            let matchedLetters =
+                "<strong>" + city.substr(0, inputValue.length) + "</strong>";
+            let otherLetters = city.substr(inputValue.length);
+            let hiddenInputValue = "<input type='hidden' value='" + city + "'>";
+
+            matchedCities.innerHTML =
+                matchedLetters + otherLetters + hiddenInputValue;
+
+            matchedCities.addEventListener("click", clickOnSuggestionHandler);
+            autoCompleteList.appendChild(matchedCities);
+            return false;
+        });
+    }
+};
+
+export { populateAutocomplete };
--- client/src/pages/OverviewPage/index.js
+++ client/src/pages/OverviewPage/index.js
@@ -1,151 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import getCitySuggestions from "../../utils/getCitySuggestions";
 import styles from "./OverviewPage.module.css";
 
 import Header from "../../components/Header";
-import FormInput from "../../components/Form/Input";
+import CitySearch from "../../components/CitySearch";
 import JobCard from "../../components/JobCard";
-
-import axios from "axios";
-
-function closeAllLists(elmnt) {
-    const allAutocompleteItems = document.getElementsByClassName(
-        "autocomplete-items"
-    );
-    for (let item of allAutocompleteItems) {
-        item.parentNode.removeChild(item);
-    }
-
-    let autoCompleteList = document.getElementById("City-autocomplete-list");
-    if (autoCompleteList)
-        autoCompleteList.parentNode.removeChild(autoCompleteList);
-}
+import LoadingWheel from "../../components/LoadingWheel";
 
 const OverviewPage = () => {
     const [citiesData, setCitiesData] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-    const [inputError, setInputError] = useState(false);
     const [fetchingJobData, setFetchingJobData] = useState(false);
     const [jobsData, setJobsData] = useState([]);
 
-    // Conditonals
-
-    const inputValueNotBlank = inputValue !== "";
     const jobsDataIsPopulated = jobsData.length > 0;
 
-    useEffect(() => {
-        console.log(jobsData);
-    }, [jobsData]);
-
-    useEffect(() => {
-        if (inputValueNotBlank) {
-            let matchingCities = getCitySuggestions(inputValue);
-            setCitiesData(matchingCities);
-        }
-    }, [inputValue, inputValueNotBlank]);
-
-    useEffect(() => {
-        closeAllLists();
-
-        if (inputValueNotBlank) {
-            let cityInput = document.getElementById("City");
-            let autoCompleteList = document.createElement("DIV");
-
-            autoCompleteList.setAttribute(
-                "id",
-                cityInput.id + "-autocomplete-list"
-            );
-            autoCompleteList.setAttribute(
-                "class",
-                styles["autocomplete-items"]
-            );
-
-            cityInput.parentNode.appendChild(autoCompleteList);
-
-            let autoCompleteLimit = 5;
-
-            citiesData.some((cityObj, i) => {
-                if (i >= autoCompleteLimit) return true;
-                let city = cityObj.city;
-                if (city === inputValue) return true;
-
-                let matchedCities = document.createElement("DIV");
-
-                let matchedLetters =
-                    "<strong>" +
-                    city.substr(0, inputValue.length) +
-                    "</strong>";
-                let otherLetters = city.substr(inputValue.length);
-                let hiddenInputValue =
-                    "<input type='hidden' value='" + city + "'>";
-
-                matchedCities.innerHTML =
-                    matchedLetters + otherLetters + hiddenInputValue;
-
-                matchedCities.addEventListener(
-                    "click",
-                    clickOnSuggestionHandler
-                );
-                autoCompleteList.appendChild(matchedCities);
-                return false;
-            });
-        }
-    }, [citiesData, inputValue, inputValueNotBlank]);
-
-    // Handler callbacks
-
-    const handleInputChange = (event) => setInputValue(event.target.value);
-
-    const clickOnSuggestionHandler = (event) => {
-        const newInputValue = event.target.getElementsByTagName("input")[0]
-            .value;
-
-        setInputValue(newInputValue);
-        closeAllLists();
-    };
-
-    const handleOnSubmit = async (event) => {
-        event.preventDefault();
-
-        let citySuggestions = getCitySuggestions(inputValue);
-
-        let isInputValueAValidCity = citySuggestions.some(
-            (cityObj) => cityObj.city.toLowerCase() === inputValue.toLowerCase()
-        );
-
-        if (!isInputValueAValidCity) {
-            setInputError(true);
-            return;
-        }
-        setInputError(false);
-        closeAllLists();
-
-        setJobsData([]);
-
-        setFetchingJobData(true);
-
-        try {
-            const response = await axios.get("/getJobsByCity", {
-                params: {
-                    city: inputValue
-                }
-            });
-            if (response.status !== 200) console.error("TODO handle");
-
-            setJobsData(response.data.results);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
     // Page renders
-
-    const loadingWheel = (
-        <div id="loader-container">
-            <div className={styles.loader}></div>
-        </div>
-    );
 
     const listOfJobs = (
         <section>
@@ -162,26 +31,17 @@ const OverviewPage = () => {
         </section>
     );
 
-    const citySearchForm = (
-        <form autoComplete="off" onSubmit={handleOnSubmit}>
-            <div className={styles.autocomplete}>
-                <FormInput
-                    label={"City"}
-                    handleInputChange={handleInputChange}
-                    inputValue={inputValue}
-                    isError={inputError}
-                />
-            </div>
-            <input type="submit" />
-        </form>
-    );
-
     const mainContent = (
         <main className={styles[`top-content`]}>
             <Header size={"large"} text={"Find your city"} />
-            {citySearchForm}
+            <CitySearch
+                setJobsData={setJobsData}
+                setFetchingJobData={setFetchingJobData}
+                citiesData={citiesData}
+                setCitiesData={setCitiesData}
+            />
             {fetchingJobData &&
-                (jobsDataIsPopulated ? listOfJobs : loadingWheel)}
+                (jobsDataIsPopulated ? listOfJobs : <LoadingWheel />)}
         </main>
     );
 
